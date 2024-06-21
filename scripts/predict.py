8088cbb1eddbbc433e052f0854976ff9615b0689
Yasin <148462854+YShokrollahi@users.noreply.github.com>
2024-06-21 17:03:42 -0500

    Merge branch 'release/v1.1.1' of https://github.com/idso-fa1-pathology/cell-sight-package into release/v1.1.1

--- scripts/predict.py
+++ scripts/predict.py
@@ -26,7 +26,7 @@ def predict_new_images(images_directory, weights_url, weights_path):
             h, w = image.shape[:2]
 
             if h > 640 or w > 640:
-                patches = create_patches(image, patch_size=640, overlap=0)
+                patches = create_patches(image, patch_size=640, overlap=100)
                 patch_dir = os.path.join(images_directory, f"{file_name}_patches")
                 save_patches(patches, patch_dir)
 
@@ -38,7 +38,7 @@ def predict_new_images(images_directory, weights_url, weights_path):
                 if latest_predict_dir:
                     label_dir = os.path.join(latest_predict_dir, 'labels')
                     predicted_patches = [(cv2.imread(os.path.join(patch_dir, patch_name)), patch_name, coords) for patch, patch_name, coords in patches]
-                    stitched_image = stitch_patches(predicted_patches, (h, w), label_dir, patch_size=640, overlap=0)
+                    stitched_image = stitch_patches(predicted_patches, (h, w), label_dir, patch_size=640, overlap=100)
                     output_path = os.path.join(images_directory, f"{file_name}_stitched.png")
                     cv2.imwrite(output_path, stitched_image)
             else:
