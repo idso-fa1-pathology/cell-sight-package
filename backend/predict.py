e9d70e96c625b1056b340577720282ab1b82fd15
Yasin <148462854+YShokrollahi@users.noreply.github.com>
2024-08-06 09:29:48 -0500

    Update predict.py

--- backend/predict.py
+++ backend/predict.py
@@ -17,7 +17,7 @@ def run_inference(file_paths, weights_path='backend/models/weights/best.pt'):
 
     for file_path in file_paths:
         file_name = os.path.basename(file_path).split('.')[0]
-        prediction_results = model.predict(source=file_path, save=True, imgsz=640, max_det=3000, conf=0.1, show_labels=False, save_txt=True, project=save_dir, name=file_name)
+        prediction_results = model.predict(source=file_path, save=True, imgsz=640, max_det=3000, conf=0.3, show_labels=False, save_txt=True, project=save_dir, name=file_name)
 
         # Collect output paths from each result
         for result in prediction_results:
